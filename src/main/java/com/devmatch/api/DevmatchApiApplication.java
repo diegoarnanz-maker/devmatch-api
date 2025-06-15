4a0be57adeea5d97c8537df64f0ee74e1f89a87f
diegoarnanz-maker <diegoarnanzlozano96@gmail.com>
2025-06-15 13:47:33 +0200

    chore: proyecto inicial Spring Boot DevMatch con arquitectura hexagonal

---
+++ src/main/java/com/devmatch/api/DevmatchApiApplication.java
@@ -0,0 +1,13 @@
+package com.devmatch.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevmatchApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevmatchApiApplication.class, args);
+	}
+
+}
